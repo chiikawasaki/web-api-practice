4406db68aa4629c7012ad83aefe2ff30674a697c
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-09-16 22:59:47 +0900

    style ImageGrallrey.js

---
+++ src/ImageGrallery.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ImageGrallery = () => {
+  return (
+    <div>
+      <div className="images-wrapper"></div>
+    </div>
+  );
+};
+
+export default ImageGrallery;
